5cb0b3dfd7b6ad1527194349727f724feb60bc85
patricio rojas <patricio@irricrops.com>
2021-02-17 13:41:20 -0300

    :package: manejo de categorias mediante Store

--- src/store.js
+++ src/store.js
@@ -7,16 +7,24 @@ export default new Vuex.Store({
   state: {
     baseUrl        :"http://sva.talana.com:8000/api/",
     productsInCart : [],
+    categories: [],
   },
 
   getters:{
     baseUrl        : state => state.baseUrl,
     productsInCart : state => state.productsInCart,
+    categories     : state => state.categories,
+    categoryById   : (state) => (id) => {
+      return state.categories.find(category => category.id === id)
+    },
   },
 
   mutations: {
     addProductInCart(state, productAndCount){
       state.productsInCart.push( productAndCount );
+    },
+    setCategories(state, categories){
+      state.categories = categories;
     }
   },
   actions: {
